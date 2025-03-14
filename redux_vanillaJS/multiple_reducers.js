94f9bd6a91297d2e0cd09c549bab2edb16018d13
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-03-14 11:22:26 +0530

    practiced combine reducers

--- redux_vanillaJS/multiple_reducers.js
+++ redux_vanillaJS/multiple_reducers.js
@@ -2,43 +2,78 @@ const redux = require("redux");
 
 // creating a store in redux ( for practice only)
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers; // newly learned topic
 
-const ORDER_PIZZA = "ORDER_PIZZA";
-const CANCEL_ORDER = "CANCEL_ORDER";
+const ORDER_POMEGRANATE = "ORDER_POMEGRANATE";
+const ORDER_ORANGE = "ORDER_ORANGE";
+const CANCEL_ORDER_POMEGRANATE = "CANCEL_ORDER_POMEGRANATE";
+const CANCEL_ORDER_ORANGE = "CANCEL_ORDER_ORANGE";
 
 // Action Creator
-function orderPizza() {
+function orderPomegranate() {
   return {
-    type: ORDER_PIZZA,
+    type: ORDER_POMEGRANATE,
   };
 }
 
-function cancelOrder() {
+function orderOrange() {
   return {
-    type: CANCEL_ORDER,
+    type: ORDER_ORANGE,
   };
 }
 
+function cancelOrderPomegranate() {
+  return {
+    type: CANCEL_ORDER_POMEGRANATE,
+  };
+}
+function cancelOrderOrange() {
+  return {
+    type: CANCEL_ORDER_ORANGE,
+  };
+}
 // Reducer
-const initialState = {
-  pizzaBase: 100,
+const promegranateStockState = {
+  pomegranteStock: 50,
+};
+const orangeStockState = {
+  orangeStock: 20,
 };
 
-const reducer = (state = initialState, action) => {
-  console.log("\nReducer called with action:", action.type);
+const promegranateStockReducer = (state = promegranateStockState, action) => {
   switch (action.type) {
-    case ORDER_PIZZA:
-      console.log("\nOrdering Pizaa...... Thanks for waiting ;)");
+    case ORDER_POMEGRANATE:
+      console.log("\nOrdering Pomegrante...... Thanks for waiting ;)");
 
       return {
         ...state,
-        pizzaBase: state.pizzaBase - 1,
+        pomegranteStock: state.pomegranteStock - 1,
       };
-    case CANCEL_ORDER:
+    case CANCEL_ORDER_POMEGRANATE:
       console.log("\nCanceling your Order...... visit again :(");
       return {
         ...state,
-        pizzaBase: state.pizzaBase + 1,
+        pomegranteStock: state.pomegranteStock + 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const orangeStockReducer = (state = orangeStockState, action) => {
+  switch (action.type) {
+    case ORDER_ORANGE:
+      console.log("\nOrdering Orange...... Thanks for waiting ;)");
+
+      return {
+        ...state,
+        orangeStock: state.orangeStock - 1,
+      };
+    case CANCEL_ORDER_ORANGE:
+      console.log("\nCanceling your Order...... visit again :(");
+      return {
+        ...state,
+        orangeStock: state.orangeStock + 1,
       };
     default:
       return state;
@@ -46,6 +81,11 @@ const reducer = (state = initialState, action) => {
 };
 
 // create and store our application state in redux
+
+const reducer = combineReducers({
+  promegranate: promegranateStockReducer,
+  orange: orangeStockReducer,
+});
 const store = createStore(reducer);
 
 // get current state
@@ -54,19 +94,23 @@ console.log("Intital state : ", store.getState());
 // registering listeners via subscriber(listener)
 // subscriber reutrns unsubscriber for disconnecting
 const unsubscribe = store.subscribe(() =>
-  console.log("Updated State with pizza count - ", store.getState().pizzaBase)
+  console.log("stock left - ", store.getState())
 );
 
-store.dispatch(orderPizza());
-store.dispatch(orderPizza());
-store.dispatch(orderPizza());
-store.dispatch(cancelOrder());
+store.dispatch(orderPomegranate());
+store.dispatch(orderPomegranate());
+store.dispatch(orderPomegranate());
+store.dispatch(cancelOrderPomegranate());
+
+store.dispatch(orderOrange());
+store.dispatch(orderOrange());
+store.dispatch(cancelOrderOrange());
 
 unsubscribe();
 
 //IMP*  UNSUBSCRIBED ... so no updates in application!! ( state in updated internally )
 
-store.dispatch(orderPizza());
+store.dispatch(orderPomegranate());
 console.log(
   "State after dispatching post-unsubscribe: ",
   store.getState(),
